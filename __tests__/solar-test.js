ec2431bb8c7aea145076d845fdd28b9fc9bdaad9
Tyler Bates <Fondlytyler@gmail.com>
2020-04-17 10:07:17 -0700

    successfully pass first test returning number for input age and string for planet name.

--- __tests__/solar-test.js
+++ __tests__/solar-test.js
@@ -2,10 +2,10 @@ import { Planets } from "./../src/solar-bizz.js"
 
 describe ("Planets", () => {
 
-  test('should verify user input is numbers only', () => {
-    const testOne = new Planets("33", "earth", "365")
-    expect(typeof(testOne.userAge)).toEqual(33);
-    expect(typeof(testOne.planetName)).toEqual("earth");
-    expect(typeof(testOne.userAge)).toEqual("365");
+  test('should verify user input age is numbers only', () => {
+    const testOne = new Planets( 33, "earth", 365)
+    expect(typeof(testOne.userAge)).toEqual("number");
+    expect(typeof(testOne.planetName)).toEqual("string");
+    expect(typeof(testOne.perYear)).toEqual("number");
   })
 })

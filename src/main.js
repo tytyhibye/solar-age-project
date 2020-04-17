d34f5d64de72f0c3488548ee8fff5724d3c7af1b
Tyler Bates <Fondlytyler@gmail.com>
2020-04-17 14:54:01 -0700

    convey a feeling of hopelessness as I've yet to get a webpack environment to not have errors.

--- src/main.js
+++ src/main.js
@@ -11,28 +11,32 @@ import { mercury } from "./solar-bizz.js";
 
 //UI logic:
 $(document).ready(function() {
-  let planetList = [mercury];
+  let planetList = [];
 
 
   $("form#inputForm").submit(function(event) {
     event.preventDefault();
 
     var inputAge = $("#age").val();
-    console.log(inputAge);
     var planetName = $("#planet").val();
-    console.log(planetName);
+    console.log(inputAge)
+    console.log(planetName)
     var planetAge = new Planet(inputAge, planetName);
-
+    
     this.getDays(inputAge);
     comment.log(this.perYear)
-    // getAge();
-
+    
+    // this.getAge();
+    
     this.planetSelector(planetName) {
       if (planetName === "Mercury") {
-
+        this.perYear = 88;
+      } else if(planetName === "Venus") {
+        this.perYear = 224;
       }
     }
 
-    $("#outputAge").text(this.solarAge)
+    $("#outputAge").text(this.solarAge);
+    $("#outputPlanet").text(this.planetName);
   });
 });

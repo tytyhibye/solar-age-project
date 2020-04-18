ec6b587f12974c3fce9a57c5e5ab9e0a676f6602
Tyler Bates <Fondlytyler@gmail.com>
2020-04-17 17:14:02 -0700

    remove unused code.

--- src/solar-bizz.js
+++ src/solar-bizz.js
@@ -16,12 +16,3 @@ export class Planet {
     return this.solarAge
   }
 }
-export let mercury = new Planet("Mercury", 88);
-export let venus = new Planet("Venus", 224);
-export let earth = new Planet("Earth", 365);
-export let mars = new Planet("Mars", 687);
-export let saturn = new Planet("Saturn", 10759);
-export let jupiter = new Planet("Jupiter", 4329);
-export let uranus = new Planet("Uranus", 30688.5);
-export let neptune = new Planet("Neptune", 60182);
-export let pluto = new Planet("Pluto", 90553);

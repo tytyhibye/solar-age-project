6c60701fea2dbafc05f92517e56b6a36f871b266
Tyler Bates <Fondlytyler@gmail.com>
2020-04-17 17:27:13 -0700

    re-pass test after previous changes didn't save.

--- __tests__/solar-test.js
+++ __tests__/solar-test.js
@@ -5,19 +5,19 @@ console.log(Planet)
 describe ("Planet", () => {
 
   test('should verify user input age is numbers only', () => {
-    const testOne = new Planet(planetName, perYear, usersAge)
+    const testOne = new Planet("Earth", 365, 33)
     expect(typeof(testOne.planetName)).toEqual("string");
     expect(typeof(testOne.perYear)).toEqual("number");
-    expect(typeof(testOne.inputAge)).toEqual("number");
+    expect(typeof(testOne.usersAge)).toEqual("number");
   })
 
   test('should return earth years in days', () => {
-    let testTwo = new Planet(planetName, perYear, usersAge)
-    expect(testTwo.getDays(testTwo.inputAge)).toEqual(12045);
+    let testTwo = new Planet("Earth", 365, 33)
+    expect(testTwo.getDays(testTwo.usersAge)).toEqual(12045);
   })
 
   test('should divide earth age in days by planet days per year to return exact solar age', () => {
-    let testThree = new Planet(planetName, perYear, usersAge)
-    expect(testThree.getAge(testThree.solarAge)).toEqual(136.875)
+    let testThree = new Planet("Mercury", 88, 33)
+    expect(testThree.getAge(testThree.getDays())).toEqual(136.875);
   })
 })

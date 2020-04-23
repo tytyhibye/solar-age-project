77a133e86e3e946711c9b9fbf0f0067eeb4cdd46
Tyler Bates <Fondlytyler@gmail.com>
2020-04-22 21:44:22 -0700

    confirm that webpack environment is functioning.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-//import $ from "jquery";
+import $ from "jquery";
 import { Planet } from "./solar-bizz.js";
 
 //bizz logic:

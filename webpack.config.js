c0150e94d4c8bd19dcff4db3155dadbb8e91ce13
Tyler Bates <Fondlytyler@gmail.com>
2020-05-05 15:42:01 -0700

    fix webpack environment for deployment.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'currency-exchange',
+      title: 'solar-age',
       template: './src/index.html',
       inject: 'body'
     })
